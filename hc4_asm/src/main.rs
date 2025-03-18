e5880e13e1df34aede15a13df8d26772b1be795c
rdsqC <pdiamondq49@gmail.com>
2025-03-18 19:52:19 +0900

    ファイルを一行ごとに読み込むことに成功 #2

--- hc4_asm/src/main.rs
+++ hc4_asm/src/main.rs
@@ -1,6 +1,16 @@
 use std::env;
+use std::fs::File;
+use std::io::{ BufReader, BufRead };
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    dbg!(&args);
+
+    let source_file_path = &args[1];
+    for line in BufReader::new(File::open(source_file_path)?).lines() {
+        let l = line?;
+        println!("{}", l);
+    }
+
+    Ok(())
 }
