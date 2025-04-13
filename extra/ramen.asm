b24436c1e2fc205c7337469445c178cda6c284e0
nasu8151 <nasu.tougarashi@gmail.com>
2025-04-13 18:28:12 +0900

    基板のイメージ画像出力、ラーメンタイマー修正

--- extra/ramen.asm
+++ extra/ramen.asm
@@ -6,30 +6,39 @@
 np
 ld #0xF
 ld #0
+sa r15
 sc [ab] ; mov [0xF0], #0
+ld #0xB
+sa r14
+ld #0xD
+sa r13
 ld #13
-sa r1   ; mov r1, #13
+sa r1   ; mov 
 ld #9
-sa r0   ; mov r0, #9
+sa r0
 ld r0
 ld #1
-ad r0   ; add r0, #1, r0
-ld #0x0
-ld #0x0
-ld #0x8
-jp nc   ; b nc 0x008
-ld r1
+ad r0
+ld r15
+ld r15
+ld r13
+jp nc
 ld #1
-ad r1   ; add r1, #1, r1
+ld r1
+ad r1
+ld r15
+ld r15
+ld r14
+jp nc
+ld #0xF
+ld #0xF
 ld #0x0
+sc [ab]
+ld #0
+ld #0xF
+ld #0
+sc [AB]
 ld #0x0
+ld #0x2
 ld #0x6
-jp nc   ; b nc 0x006
-ld #0xF
-ld #0xF
-ld #0x0
-sc [ab] ; mov [0xF0], #0xF
-ld #0x0
-ld #0x1
-ld #0xD
-jp      ; halt
+jp
