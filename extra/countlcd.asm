7c7b9fc11ed1ff2a105df3567b33bccafffe350f
nasu8151 <nasu.tougarashi@gmail.com>
2025-09-28 12:17:26 +0900

    オープニングメッセージを変更、4桁に対応

--- extra/countlcd.asm
+++ extra/countlcd.asm
@@ -208,32 +208,19 @@ setup9:
     jp
 
 data:
-    li #0b1011
-    li #0xF
-    li #0x3
-    sm
-    li #0b0010
-    li #0xF
-    li #0x3
-    sm            ; Display "ｲ"
-
     li #0
     sa r4
     li #0
     sa r5
     li #0
     sa r6
+    li #0
+    sa r7         ; Clear digit registers
 
-    li #main:2
-    sa r15
-    li #main:1
-    sa r14
-    li #main:0
-    sa r13
-    li #wait_264us:2
-    li #wait_264us:1
-    li #wait_264us:0
-    jp            ; Wait for HD44780 to finish
+    li #opening_message:2
+    li #opening_message:1
+    li #opening_message:0
+    jp
 
 main:
     li #0xc
@@ -244,6 +231,26 @@ main:
     li #0xF
     li #0x2
     sm            ; Set DDRAM address to 0x40 (2nd line, pos 0)
+
+    li #write1000:2
+    sa r15
+    li #write1000:1
+    sa r14
+    li #write1000:0
+    sa r13
+    li #wait_264us:2
+    li #wait_264us:1
+    li #wait_264us:0
+    jp            ; Wait for HD44780 to finish
+write1000:
+    li #0x3
+    li #0xF
+    li #0x3
+    sm
+    ld r7
+    li #0xF
+    li #0x3
+    sm            ; Display thousands digit
 
     li #write100:2
     sa r15
@@ -354,10 +361,6 @@ poll:
     jp nc         ; If not carry, skip reset
     li #0
     sa r5         ; Reset tens digit to 0
-    li #0xA
-    li #0xF
-    li #0x0
-    sm            ; Output 5V to buzzer at 0xF0
     li #1
     ld r6
     ad r6         ; Increment hundreds digit
@@ -370,10 +373,33 @@ poll:
     jp nc         ; If not carry, skip reset
     li #0
     sa r6         ; If carry, reset hundreds digit to 0
+    li #1
+    ld r7
+    ad r7         ; Increment thousands digit
+    li #6
+    ld r7
+    ad r15        ; If r7 > 10, carry flag will be set
+    li #main:2
+    li #main:1
+    li #main:0
+    jp nc         ; If not carry, skip reset
+    li #0
+    sa r7         ; If carry, reset thousands digit to 0
     li #0x5
     li #0xF
     li #0x0
     sm            ; Output 5V to buzzer at 0xF0
+    li #done:2
+    sa r15
+    li #done:1
+    sa r14
+    li #done:0
+    sa r13
+    li #wait_100ms:2
+    li #wait_100ms:1
+    li #wait_100ms:0
+    jp            ; Wait 100ms
+done:
     li #main:2
     li #main:1
     li #main:0
@@ -514,3 +540,122 @@ wait_1ms_ret:
     ld r13
     jp
 
+opening_message:
+    li #0b0100
+    li #0xF
+    li #0x3
+    sm
+    li #0b1000
+    li #0xF
+    li #0x3
+    sm            ; Display "H"
+    li #msg1:2
+    sa r15
+    li #msg1:1
+    sa r14
+    li #msg1:0
+    sa r13
+    li #wait_264us:2
+    li #wait_264us:1
+    li #wait_264us:0
+    jp            ; Wait for HD44780 to finish
+msg1:
+    li #0b0110
+    li #0xF
+    li #0x3
+    sm
+    li #0b0101
+    li #0xF
+    li #0x3
+    sm            ; Display "e"
+    li #msg2:2
+    sa r15
+    li #msg2:1
+    sa r14
+    li #msg2:0
+    sa r13
+    li #wait_264us:2
+    li #wait_264us:1
+    li #wait_264us:0
+    jp            ; Wait for HD44780 to finish
+msg2:
+    li #0b0110
+    li #0xF
+    li #0x3
+    sm
+    li #0b1100
+    li #0xF
+    li #0x3
+    sm            ; Display "l"
+    li #msg3:2
+    sa r15
+    li #msg3:1
+    sa r14
+    li #msg3:0
+    sa r13
+    li #wait_264us:2
+    li #wait_264us:1
+    li #wait_264us:0
+    jp            ; Wait for HD44780 to finish
+msg3:
+    li #0b0110
+    li #0xF
+    li #0x3
+    sm
+    li #0b1100
+    li #0xF
+    li #0x3
+    sm            ; Display "l"
+    li #msg4:2
+    sa r15
+    li #msg4:1
+    sa r14
+    li #msg4:0
+    sa r13
+    li #wait_264us:2
+    li #wait_264us:1
+    li #wait_264us:0
+    jp            ; Wait for HD44780 to finish
+msg4:
+    li #0b0110
+    li #0xF
+    li #0x3
+    sm
+    li #0b1111
+    li #0xF
+    li #0x3
+    sm            ; Display "o"
+    li #msg5:2
+    sa r15
+    li #msg5:1
+    sa r14
+    li #msg5:0
+    sa r13
+    li #wait_264us:2
+    li #wait_264us:1
+    li #wait_264us:0
+    jp            ; Wait for HD44780 to finish
+msg5:
+    li #0b0010
+    li #0xF
+    li #0x3
+    sm
+    li #0b0001
+    li #0xF
+    li #0x3
+    sm            ; Display "!"
+    li #msg6:2
+    sa r15
+    li #msg6:1
+    sa r14
+    li #msg6:0
+    sa r13
+    li #wait_264us:2
+    li #wait_264us:1
+    li #wait_264us:0
+    jp            ; Wait for HD44780 to finish
+msg6:
+    li #main:2
+    li #main:1
+    li #main:0
+    jp
