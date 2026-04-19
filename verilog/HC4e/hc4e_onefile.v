4e3316c5a220828b72b3adbbf156a017e110a54f
nasu8151 <nasu.tougarashi@gmail.com>
2026-04-19 09:33:07 +0900

    feat: HC4eの新しいモジュールを追加し、ALUとメモリ操作を実装

--- verilog/HC4e/hc4e_onefile.v
+++ verilog/HC4e/hc4e_onefile.v
@@ -1,11 +1,10 @@
 module hc4 (
     input wire clk,
     input wire rst_n,
-    output wire [11:0]      pc_out,
+    output wire [7:0]      pc_out,
     output wire [3:0]       stackA_out,
     output wire [3:0]       stackB_out,
-    output wire [3:0]       stackC_out,
-    output wire [7:0]       address_bus,
+    output wire [3:0]       address_bus,
     inout  wire [3:0]       data_bus,
     output wire             rd_n,
     output wire             wr_n
@@ -14,10 +13,10 @@ module hc4 (
     // Internal signals
     reg [3:0] level_A; //stack level A
     reg [3:0] level_B; //stack level B
-    reg [3:0] level_C; //stack level C
-    reg [11:0] pc;
+    // reg [3:0] level_C; //stack level C
+    reg [7:0] pc;
 
-    reg [7:0] rom [0:4095];
+    reg [7:0] rom [0:255];
 
     initial $readmemh("./jmptest.hex", rom);
 
@@ -29,7 +28,6 @@ module hc4 (
     reg [3:0] alu_result;
     reg  carry_out;
     reg  carry_flg;
-    reg  zero_flg;
 
     assign instruction = rom[pc];
     assign pc_out = pc;
@@ -39,8 +37,8 @@ module hc4 (
 
     assign stackA_out = level_A;
     assign stackB_out = level_B;
-    assign stackC_out = level_C;
-    assign address_bus = (opcode == 4'b0000 || opcode == 4'b1000) ? {level_A, level_B} : {4'b0000, oprand};
+    // assign stackC_out = level_C;
+    assign address_bus = oprand; //address bus is used for memory access with immediate operand
     assign data_bus = (opcode[3] == 1'b0) ? alu_result : 4'bz; //write to data bus for ALU output or immediate data
 
     assign wr_n = !(!opcode[3] & !clk);
@@ -54,30 +52,28 @@ module hc4 (
 
     // ALU module
     always @(*) begin
-        casez (opcode[2:0])
-            3'b00?: alu_result = level_C; //SM, SC
-            3'b010: {carry_out, alu_result} = level_A - level_B; //SU
-            3'b011: {carry_out, alu_result} = level_A + level_B; //AD
-            3'b100: alu_result = level_A ^ level_B; //XR
-            3'b101: alu_result = level_A | level_B; //OR
-            3'b110: alu_result = level_A & level_B; //AN
-            3'b111: alu_result = level_A;           //SA
+        casez (opcode)
+            // 4'b000?: alu_result = level_C; //SM, SC
+            4'b0010: {carry_out, alu_result} = level_A - level_B; //SU
+            4'b0011: {carry_out, alu_result} = level_A + level_B; //AD
+            4'b0100: alu_result = level_A ^ level_B; //XR
+            4'b0101: alu_result = level_A | level_B; //OR
+            4'b0110: alu_result = level_A & level_B; //AN
+            4'b0111: alu_result = level_A;           //SA
             default: alu_result = 4'b0000;
         endcase
     end
     // Main sequential logic
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
-            pc <= 12'b0;
-            level_A <= 4'b0;
+            pc <= 8'b0;
+            // level_A <= 4'b0;
             // level_B <= 4'b0;
             // level_C <= 4'b0;
             carry_flg <= 1'b0;
-            zero_flg <= 1'b0;
         end else begin
             // Execute instruction based on opcode
             if (stack_op) begin
-                zero_flg <= (data_bus == 4'b0000) ? 1'b1 : 1'b0;
                 if (opcode == 4'b0010 || opcode == 4'b0011) begin
                     carry_flg <= carry_out;
                 end
@@ -85,21 +81,19 @@ module hc4 (
             if (lm_ld) begin
                 level_A <= data_bus;
                 level_B <= level_A;
-                level_C <= level_B;
+                // level_C <= level_B;
             end
             if (li) begin
                 level_A <= oprand;
                 level_B <= level_A;
-                level_C <= level_B;
+                // level_C <= level_B;
             end
             if (jp_np) begin //JP, NP
                 case (instruction[2:0])
-                    3'b000: pc <= {level_C, level_B, level_A};              // JP
+                    3'b000: pc <= {level_B, level_A};              // JP
                     3'b001: pc <= pc + 1;              // NP
-                    3'b010: pc <= (carry_flg == 1) ? {level_C, level_B, level_A} : pc + 1; // JP C
-                    3'b011: pc <= (carry_flg == 0) ? {level_C, level_B, level_A} : pc + 1; // JP NC
-                    3'b100: pc <= (zero_flg == 1) ? {level_C, level_B, level_A} : pc + 1;  // JP Z
-                    3'b101: pc <= (zero_flg == 0) ? {level_C, level_B, level_A} : pc + 1;  // JP NZ
+                    3'b010: pc <= (carry_flg == 1) ? {level_B, level_A} : pc + 1; // JP C
+                    3'b011: pc <= (carry_flg == 0) ? {level_B, level_A} : pc + 1; // JP NC
                     default: pc <= pc + 1;
                 endcase
             end else begin
